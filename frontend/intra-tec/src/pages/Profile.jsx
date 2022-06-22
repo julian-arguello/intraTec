98e1fe6ed49baa92d98a33d6096d001e3218ae8b
Seba MG <sebastian.gariglio@davinci.edu.ar>
2022-06-21 21:25:40 -0300

    profile boxes

--- frontend/intra-tec/src/pages/Profile.jsx
+++ frontend/intra-tec/src/pages/Profile.jsx
@@ -1,12 +1,14 @@
 import imagenes from '../assets/images';
-function Profile(props){
 
-    return(
-        
-        <div className="container pt-4">
-            <h2>Perfil de Técnico</h2>
-            <div className="pb-4 mt-4">
-                <button className="btn btn-primary" to="#">Editar perfil</button>
+function Profile(props){
+    return(        
+        <div className="container">
+
+            <div className=" d-flex justify-content-between align-items-center">
+ 
+                <h2>Perfil de Técnico</h2>
+                <button className="btn btn-outline-primary" to="#">Editar perfil</button>
+
             </div>           
                            
             <div className="row">
@@ -33,17 +35,17 @@ function Profile(props){
 
                         <br className="p-1 d-md-none" />
 
-                        <div className="d-flex justify-content-end">
-                            <li className='me-4'>
+                        <div className="d-md-flex justify-content-center justify-content-md-end">
+                            <div className='me-4'>
                                 <p className=" text-dark badge p-0">Cantidad de equipos recepcionados</p>
                                 <br />
                                 <p className="text-center p-2 text-white box-proceso rounded-4 border-0 h3">244</p>
-                            </li>
-                            <li>
+                            </div>
+                            <div className='me-4'>
                                 <p className=" text-dark badge p-0">Cantidad de equipos reparados</p>
                                 <br />
                                 <p className="text-center p-2 rounded-4 text-white box-reparados rounded-2 border-0 h3">134</p>
-                            </li>
+                            </div>
                         </div>
                     </ul>
                 </div>
@@ -105,6 +107,10 @@ function Profile(props){
                     </table>
                 </div> */}
             </div>
+
+
+
+
         </div>
     )
 }
