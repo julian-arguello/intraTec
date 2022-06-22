72c0382e2c8ac4a1a0ae17bda5c2f0b5945fa369
Julián Argüello <julian.andres.arguello@gmail.com>
2022-06-21 22:46:30 -0300

    resolve conflict

--- frontend/intra-tec/src/pages/Home.jsx
+++ frontend/intra-tec/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useService }  from '../context/Service.Context';
 import { Link } from 'react-router-dom';
-import imagenes from '../assets/images';
+// import imagenes from '../assets/images';
 
 
 import imagenes from '../assets/images';
@@ -16,28 +16,28 @@ function Home(props){
                 <div className="box-recepcionados p-3 m-1 rounded-4 col-5 col-lg-2 size-box-stadistics">
                     <div  className="d-flex align-items-center">
                         <p className="h3"><strong>10</strong></p>
-                        <img src={ imagenes.recepcionado } alt="recepcion-logo" className="stadistic h-100"/>
+                        <img src={'#'/*imagenes.recepcionado*/ } alt="recepcion-logo" className="stadistic h-100"/>
                     </div>
                     <p className="m-0"><strong>Recepcionados</strong></p>
                 </div>
                 <div className="box-proceso p-3 m-1 rounded-4 col-5 col-lg-2 size-box-stadistics">
                     <div  className="d-flex align-items-center">
                         <p className="h3"><strong>10</strong></p>
-                        <img src={ imagenes.proceso } alt="enproceso-logo" className="stadistic h-100"/>
+                        <img src={'#'/*imagenes.proceso*/ } alt="enproceso-logo" className="stadistic h-100"/>
                     </div>
                     <p className="m-0"><strong>En Proceso</strong></p>
                 </div>
                 <div className="box-reparados p-3 m-1 rounded-4 col-5 col-lg-2 size-box-stadistics">
                     <div  className="d-flex align-items-center">
                         <p className="h3"><strong>10</strong></p>
-                        <img src={ imagenes.reparado } alt="reparados-logo" className="stadistic h-100"/>
+                        <img src={'#'/*imagenes.reparado*/ } alt="reparados-logo" className="stadistic h-100"/>
                     </div>
                     <p className="m-0"><strong>Reparados</strong></p>
                 </div>
                 <div className="box-sinreparacion p-3 m-1 rounded-4 col-5 col-lg-2 size-box-stadistics">
                     <div className="d-flex align-items-center">
                         <p className="h3"><strong>10</strong></p>
-                        <img src={ imagenes.sinreparar } alt="sinreparacion-logo" className="stadistic h-100"/>
+                        <img src={'#'/*imagenes.sinreparar*/ } alt="sinreparacion-logo" className="stadistic h-100"/>
                     </div>
                     <p className="m-0"><strong>Sin Reparación </strong></p>
                 </div>
