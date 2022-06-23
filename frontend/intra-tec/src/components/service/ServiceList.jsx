caa2ec5d3dae8fda6b7c3364208d8fe8326c9b2a
Julián Argüello <julian.andres.arguello@gmail.com>
2022-06-23 00:00:05 -0300

    reverse service

--- frontend/intra-tec/src/components/service/ServiceList.jsx
+++ frontend/intra-tec/src/components/service/ServiceList.jsx
@@ -11,7 +11,7 @@ export function ServiceList(){
 
     return(    
         <div className="row row-cols-1 row-cols-lg-2 row-cols-xxl-3 gy-5 gx-0 gx-sm-5 px-md-5">
-            {state.services.map((service)=>(
+            {state.services.reverse().map((service)=>(
                 <ServiceItem key={service._id} service={service}/>
             ))}
         </div>
