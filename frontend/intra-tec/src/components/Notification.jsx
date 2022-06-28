caca7bf0fefd01b201cc7801a228c7cbe7191400
alessandro <ale.signo@yahoo.com.ar>
2022-06-28 15:45:23 -0300

    fix width notification

--- frontend/intra-tec/src/components/Notification.jsx
+++ frontend/intra-tec/src/components/Notification.jsx
@@ -8,7 +8,7 @@ export function Notification(){
         <>
         {state.notification.hasOwnProperty('msj') && 
         
-        <div className="alert alert-success w-100 w-sm-50 m-auto d-flex justify-content-between align-items-center notification" role="alert">
+        <div className="alert alert-success m-auto d-flex justify-content-between align-items-center notification" role="alert">
             {state.notification.msj}
             <span className="icon-sin-reparacion" role="button" onClick={()=>(notify({}))}></span>
         </div>
