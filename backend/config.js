8aaefe5ee99ce4d7f3a58b1866231a60a3fd9c32
Julián Argüello <julian.andres.arguello@gmail.com>
2022-07-05 19:53:45 -0300

    db intratec

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,7 @@ export default {
     db: {
         host: process.env.NODE_MONGO_HOST || 'localhost',
         port: process.env.NODE_MONGO_PORT || 27017,
-        dbName: process.env.NODE_MONGO_DB || 'technical_assistant'
+        dbName: process.env.NODE_MONGO_DB || 'intratec'
     },
     jwt_key: {
         key: process.env.JWT_KEY || '463ca1b82b92aa98e76b6720abc5daf122f6e29a'
